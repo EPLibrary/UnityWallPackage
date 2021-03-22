18aabe35bda48e334e97121b2866d688e5c39a88
haleyrequa <haleyrequa@gmail.com>
2021-03-22 10:01:33 -0600

    Adding tools

---
+++ Assets/Scripts/CommandLineArguments.cs
@@ -0,0 +1,162 @@
+﻿
+using System;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+
+namespace QUT
+{
+    /// <summary>
+    /// A helper class that provides a robust mechanism for retrieving and querying command line parameters provided to the application
+    /// </summary>
+    public class CommandLineArguments
+    {
+        /// <summary>
+        /// Dictionary used to hold command line argument values
+        /// </summary>
+        StringDictionary parameters;
+
+        public CommandLineArguments(string[] Args, bool printArgs = false)
+        {
+            parameters = new StringDictionary();
+            Regex Spliter = new Regex(@"^-{1,2}|^/|=|:",
+                RegexOptions.IgnoreCase);// | RegexOptions.Compiled);
+
+            Regex Remover = new Regex(@"^['""]?(.*?)['""]?$",
+                RegexOptions.IgnoreCase);// | RegexOptions.Compiled);
+
+            string Parameter = null;
+            string[] Parts;
+
+            // Valid parameters:
+            // {-,/,--}param{ ,=,:}((",')value(",'))
+            // Examples: 
+            // -param1 value1 --param2 /param3:"Test-:-work" 
+            //   /param4=happy -param5 '--=nice=--'
+            foreach (string Txt in Args)
+            {
+                // Look for new parameters (-,/ or --) and a
+                // possible enclosed value (=,:)
+                Parts = Spliter.Split(Txt, 3);
+
+                switch (Parts.Length)
+                {
+                    // Found a value (for the last parameter 
+                    // found (space separator))
+                    case 1:
+                        if (Parameter != null)
+                        {
+                            if (!parameters.ContainsKey(Parameter))
+                            {
+                                Parts[0] =
+                                    Remover.Replace(Parts[0], "$1");
+
+                                parameters.Add(Parameter, Parts[0]);
+                            }
+                            Parameter = null;
+                        }
+                        // else Error: no parameter waiting for a value (skipped)
+                        break;
+
+                    // Found just a parameter
+                    case 2:
+                        // The last parameter is still waiting. 
+                        // With no value, set it to true.
+                        if (Parameter != null)
+                        {
+                            if (!parameters.ContainsKey(Parameter))
+                                parameters.Add(Parameter, "true");
+                        }
+                        Parameter = Parts[1];
+                        break;
+
+                    // Parameter with enclosed value
+                    case 3:
+                        // The last parameter is still waiting. 
+                        // With no value, set it to true.
+                        if (Parameter != null)
+                        {
+                            if (!parameters.ContainsKey(Parameter))
+                                parameters.Add(Parameter, "true");
+                        }
+
+                        Parameter = Parts[1];
+
+                        // Remove possible enclosing characters (",')
+                        if (!parameters.ContainsKey(Parameter))
+                        {
+                            Parts[2] = Remover.Replace(Parts[2], "$1");
+                            parameters.Add(Parameter, Parts[2]);
+                        }
+
+                        Parameter = null;
+                        break;
+                }
+            }
+            // In case a parameter is still waiting
+            if (Parameter != null)
+            {
+                if (!parameters.ContainsKey(Parameter))
+                    parameters.Add(Parameter, "true");
+            }
+
+            // Print out arguments if requested
+            if (printArgs && parameters.Count > 0)
+            {
+                Console.WriteLine("Program arguments:");
+                Console.WriteLine("--------------------------");
+                foreach (String key in parameters.Keys)
+                    Console.WriteLine(key + " = " + parameters[key]);
+                Console.WriteLine("--------------------------\n");
+            }
+        }
+
+        // Retrieve a parameter value if it exists 
+        // (overriding C# indexer property)
+        public string this[string Param]
+        {
+            get
+            {
+                return (parameters[Param]);
+            }
+        }
+
+        
+        public static T CommandLineArgument<T>(string param)
+        {
+            string arg = null;
+            QUT.CommandLineArguments cmdArgs = new QUT.CommandLineArguments(Environment.GetCommandLineArgs(), true);
+
+            try
+            {
+                if (cmdArgs != null)
+                    arg = cmdArgs[param];
+
+                if (arg != null)
+                    return (T)System.Convert.ChangeType(arg, typeof(T));
+            }
+            catch (System.Exception e)
+            {
+                UnityEngine.Debug.LogWarningFormat("WARNING: Parsing arguments failed, using default values ({0})\n", e.Message);
+
+                return (T)System.Convert.ChangeType("0", typeof(T));
+            }
+            return (T)System.Convert.ChangeType("0", typeof(T));
+        }
+
+        public static bool HasArgument(string param)
+        {
+            if (CommandLineArgument<string>(param) == "true") return true;
+
+            string[] args = Environment.GetCommandLineArgs();
+            
+            for (int i = 0; i < args.Length; ++i)
+            {
+                if (args[i] != null && args.Length > 0)
+                {
+                    if (args[i] == param) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/PositionWindow.cs
@@ -0,0 +1,123 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// A component to position/resize the current active OS window
+/// Window width [-w], height [-h], x position [-x] and y-position [-y] are specified via command line arguments
+/// The specified values are applied to the window with the current active focus on Start
+/// </summary>
+public class PositionWindow : MonoBehaviour
+{
+
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
+
+    // Import required OS Window functions from user32.dll
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+    static extern bool SetWindowPos(IntPtr hwnd, int hWndInsertAfter, int x, int y, int w, int h, int wFlags);
+
+    [DllImport("user32.dll", EntryPoint = "FindWindow")]
+    static extern IntPtr FindWindow(System.String className, System.String windowName);
+
+    [DllImport("user32.dll", EntryPoint = "GetForegroundWindow")]
+    static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll", EntryPoint = "GetWindowText")]
+    static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowText")]
+    static extern int SetWindowText(IntPtr hWnd, System.String text);
+
+    /// <summary>
+    /// Get the title of the currently active OS window
+    /// </summary>
+    /// <returns>The title of the currently active OS window</returns>
+    private string GetActiveWindowTitle()
+    {
+        const int nChars = 256;
+        StringBuilder Buff = new StringBuilder(nChars);
+        IntPtr handle = GetForegroundWindow();
+
+        if (GetWindowText(handle, Buff, nChars) > 0)
+        {
+            return Buff.ToString();
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Set the size and position of the current active window that has focus
+    /// </summary>
+    /// <param name="x">The new position of the left side of the window, in client coordinates</param>
+    /// <param name="y">The new position of the top of the window, in client coordinates</param>
+    /// <param name="w">The new width of the window, in pixels</param>
+    /// <param name="h">The new height of the window, in pixels</param>
+    public void SetPosition(int x, int y, int w = 0, int h = 0)
+    {
+        // Get the title of the currently focused window
+        string name = GetActiveWindowTitle();
+
+        // Update the window position and size
+        SetWindowPos(FindWindow(null, name), 0, x, y, w, h, w * h == 0 ? 1 : 0);        
+    }
+
+#endif
+
+    void Start()
+    {
+        try
+        {
+            int x = -1, y = -1;
+            int w = 0, h = 0;
+
+            // Retrieve command line arguments specified at launch
+            // NOTE: This is an alternative/native approach to the CommandLineArguments helper class
+            string arg;
+            string[] args = Environment.GetCommandLineArgs();
+          
+            for(int i=0; i<args.Length; ++i)
+            {
+                arg = args[i];
+
+                if (arg != null && args.Length > 0)
+                {
+                    switch(arg)
+                    {
+                        case "-x":
+                            if(args.Length > i+1)
+                                x = int.Parse(args[++i]);
+                            break;
+
+                        case "-y":
+                            if (args.Length > i + 1)
+                                y = int.Parse(args[++i]);
+                            break;
+
+                        case "-w":
+                            if (args.Length > i + 1)
+                                w = int.Parse(args[++i]);
+                            break;
+
+                        case "-h":
+                            if (args.Length > i + 1)
+                                h = int.Parse(args[++i]);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            if(x != -1 && y != -1)
+                SetPosition(x, y, w, h);
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarningFormat("WARNING: Parsing arguments failed, using default values ({0})\n", e.Message);
+        }
+    }
+}
